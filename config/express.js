de8d852de9828e6e461f2e0fa17093f3a652727a
mziyambi <mziyam@mtc.ca.gov>
2015-08-18 14:22:26 -0700

    [Client] Added bootstrap and jQuery

--- config/express.js
+++ config/express.js
@@ -30,6 +30,8 @@ module.exports = function(app) {
     app.set('views', path.join(__dirname, './../views'));
     app.set('view engine', 'ejs');
 
+    app.use(express.static(path.join(__dirname, './../public')));
+
     // Enables CORS
     var enableCORS = function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
