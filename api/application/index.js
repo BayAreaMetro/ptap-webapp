f6eaa22bfd42b5398599d7fb43a2177921d500a5
Gilbert frausto <gilbertfrausto@gmail.com>
2015-08-27 11:06:43 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	views/pages/index.ejs

--- api/application/index.js
+++ api/application/index.js
@@ -9,7 +9,12 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.post('/', controller.create);
-router.post('/:id', controller.update);
+router.post('/update2/:id', controller.update2);
+router.post('/update3a/:id', controller.update3a);
+router.post('/update3b/:id', controller.update3b);
+router.post('/update3c/:id', controller.update3c);
+router.post('/update4/:id', controller.update4);
+router.post('/update5/:id', controller.update5);
 router.get('/remove/:id', controller.destroy);
 router.get('/download', controller.download);
 
