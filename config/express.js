18f9e4e6e137daa61b8a9dd0d72bca9dd55c2779
mziyambi <mziyam@mtc.ca.gov>
2015-09-21 14:13:22 -0700

    [Global] Added live reload

--- config/express.js
+++ config/express.js
@@ -125,17 +125,17 @@ module.exports = function(app) {
 
 
     if ('production' === env) {
-        //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-        //app.use(express.static(path.join(config.root, 'public')));
-        //app.set('appPath', config.root + '/public');
+        app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+        app.use(express.static(path.join(config.root, 'public')));
+        app.set('appPath', config.root + '/public');
         app.use(logger('dev'));
     }
 
     if ('development' === env || 'test' === env) {
-        //app.use(require('connect-livereload')());
-        //app.use(express.static(path.join(config.root, '.tmp')));
-        //app.use(express.static(path.join(config.root, 'client')));
-        //app.set('appPath', 'client');
+        app.use(require('connect-livereload')());
+        app.use(express.static(path.join(config.root, '.tmp')));
+        app.use(express.static(path.join(config.root, 'client')));
+        app.set('appPath', 'client');
         app.use(logger('dev'));
         app.use(errorHandler()); // Error handler - has to be last
     }
