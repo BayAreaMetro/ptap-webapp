0189935c4ae33421c585b60a647b6da9b85025a0
mziyambi <mziyam@mtc.ca.gov>
2015-10-01 13:17:19 -0700

    [Config] Updates

--- config/environment/production.js
+++ config/environment/production.js
@@ -6,22 +6,23 @@ console.log('loading production database');
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+    // Server IP
+    ip: process.env.OPENSHIFT_NODEJS_IP ||
+        process.env.IP ||
+        undefined,
 
-  // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+    // Server port
+    port: process.env.OPENSHIFT_NODEJS_PORT ||
+        process.env.PORT ||
+        8080,
 
-  // MongoDB connection options
-  mongo: {
-    uri:    process.env.MONGOLAB_URL ||
+    // MongoDB connection options
+    mongo: {
+        uri: process.env.MONGOLAB_URL ||
             process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME 
-	        
-  }
-  
+            process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://ptap:GISptap101@ds035613.mongolab.com:35613/ptap-production'
+
+    }
+
 };
